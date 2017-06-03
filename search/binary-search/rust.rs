ea749cc1708614fd2b2b75882df62823ab44d7f8
Afshin Mehrabani <afshin.meh@gmail.com>
2017-06-03 09:34:17 +0100

    fixing some typos, adding test

--- search/binary-search/rust.rs
+++ search/binary-search/rust.rs
@@ -1,4 +1,4 @@
-fn binary_search(v: vec<i32>, target: i32) -> option<i32> {
+fn binary_search(v: &Vec<i32>, target: i32) -> Option<i32> {
     let mut min: usize = 0;
     let mut max: usize = v.len() - 1;
     let mut found: Option<i32> = None;
@@ -21,4 +21,13 @@ fn binary_search(v: vec<i32>, target: i32) -> option<i32> {
     }
 
     found
+}
+
+#[test]
+fn test_search() {
+    let v: Vec<i32> = vec![1,2,3,4];
+
+    assert_eq!(binary_search(&v, 2), Some(2));
+    assert_eq!(binary_search(&v, 5), None);
+    assert_eq!(binary_search(&v, 4), Some(4));
 }
